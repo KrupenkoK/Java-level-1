3e7e0f489dc412579233f65135e2dd119175222d
KrupenkoK <krupenkokonstantin@gmail.com>
2021-05-29 10:14:35 +0300

    update new homework4

--- HomeWork4/src/Main.java
+++ HomeWork4/src/Main.java
@@ -33,7 +33,7 @@ public class Main {
 
     //7.Выводим поле на консоль
     public static void publishField() {
-        System.out.println("-------");
+        System.out.println("- - - - - -");
         for (int i = 0; i < SIZELINE; i++) {
             System.out.print("|");
             for (int j = 0; j < SIZELINE; j++) {
@@ -41,7 +41,7 @@ public class Main {
             }
             System.out.println();
         }
-        System.out.println("-------");
+        System.out.println("- - - - - -");
     }
 
     //8. Указываем символы
@@ -74,7 +74,56 @@ public class Main {
     public static void botTurn() {
         int x;
         int y;
-        
+
+        //Блокировка победы машиной
+        for (int i = 0; i < SIZELINE; i++) {
+            for (int j = 0; j < SIZELINE; j++) {
+
+                //по горизонтали
+                if (horizontalChecker(i, j, PLAYER) == WINCOUNT - 1) {
+                    writeSymbolOnHorizontal(i, j, BOT);
+                }
+                //по вертикали
+                if (verticalChecker(i, j, PLAYER) == WINCOUNT - 1) {
+                    writeSymbolOnVertical(i, j, BOT);
+                }
+                //по верхней диагонали
+                if (diagonallUpChecker(i, j, PLAYER) == WINCOUNT - 1) {
+                    writeSymbolOnDiagonallUp(i, j, BOT);
+                }
+                //по нижней диагонали
+                if (diagonalDownChecker(i, j, PLAYER) == WINCOUNT - 1) {
+                    writeSymbolOnDiagonallDown(i, j, BOT);
+                }
+
+            }
+        }
+
+
+        //Победа машины
+        for (int i = 0; i < SIZELINE; i++) {
+            for (int j = 0; j < SIZELINE; j++) {
+
+                //по горизонтали
+                if (horizontalChecker(i, j, BOT) == WINCOUNT - 1) {
+                    writeSymbolOnHorizontal(i, j, BOT);
+                }
+                //по вертикали
+                if (verticalChecker(i, j, BOT) == WINCOUNT - 1) {
+                    writeSymbolOnVertical(i, j, BOT);
+                }
+                //по верхней диагонали
+                if (diagonallUpChecker(i, j, BOT) == WINCOUNT - 1) {
+                    writeSymbolOnDiagonallUp(i, j, BOT);
+                }
+                //по нижней диагонали
+                if (diagonalDownChecker(i, j, BOT) == WINCOUNT - 1) {
+                    writeSymbolOnDiagonallDown(i, j, BOT);
+                }
+            }
+        }
+
+
         do {
             x = rd.nextInt(SIZELINE);
             y = rd.nextInt(SIZELINE);
@@ -82,100 +131,126 @@ public class Main {
         enterSymbols(y, x, BOT);
     }
 
+    //Проверка серии побед по горизонтали
+    public static int horizontalChecker(int v, int h, char sym) {
+        int count = 0;
+        for (int i = h; i < WINCOUNT ; i++) {
+            if (field[v][i] == sym) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    //Проверка серии побед по вертикали
+    public static int verticalChecker(int v, int h, char sym) {
+        int count = 0;
+        for (int i = v; i < WINCOUNT; i++) {
+            if (field[i][h] == sym) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    //Проверка серии побед по верхней диагонали
+    public static int diagonallUpChecker(int v, int h, char sym) {
+        int count = 0;
+        for (int i = 0; i < WINCOUNT; i++) {
+            if (v - i == -1 || h + i >= SIZELINE) {
+                break;
+            }
+            if (field[v - i][h + i] == sym) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    //Проверка серии побед по нижней диагонали
+    private static int diagonalDownChecker(int v, int h, char sym) {
+        int count=0;
+        for (int i = 0; i < WINCOUNT; i++) {
+            if (v + i >= SIZELINE || h + i >= SIZELINE) {
+                break;
+            }
+            if (field[v + i][h + i] == sym) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    //Проверка заполнения линии по горизонтали
+    private static boolean writeSymbolOnHorizontal(int v, int h, char sym) {
+        for (int i = v; i <= WINCOUNT; i++) {
+            if (field[h][i] == EMPTY) {
+                field[h][i] = sym;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Проверка заполнения линии по вертикали
+    private static boolean writeSymbolOnVertical(int v, int h, char sym) {
+        for (int i = h; i <= WINCOUNT; i++) {
+            if (field[i][v] == EMPTY) {
+                field[i][v] = sym;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Проверка заполнения линии по верхней диагонали
+    private static boolean writeSymbolOnDiagonallUp(int v, int h, char sym) {
+        for (int i = h; i <= WINCOUNT; i++) {
+            if (field[h + i][v - i] == EMPTY) {
+                field[h + i][v - i] = sym;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Проверка заполнения линии по нижней диагонали
+    private static boolean writeSymbolOnDiagonallDown(int v, int h, char sym) {
+        for (int i = h; i <= WINCOUNT; i++) {
+            if (field[h + i][v + i] == EMPTY) {
+                field[h + i][v + i] = sym;
+                return true;
+            }
+        }
+        return false;
+    }
+
+
     //12. Проверка победы
     private static boolean checkWin(char sym) {
 
-        int countHorizontal = 0;
-        int countVertical = 0;
-        int coutSmallTopCrossLine = 0;
-        int coutSmallBottomCrossLine = 0;
-        int coutSmallBottomAnotherCrossLine = 0;
-        int coutSmallTopAnotherCrossLine = 0;
-
         for (int i = 0; i < SIZELINE; i++) {
             for (int j = 0; j < SIZELINE; j++) {
-
-                //Победа по горизонтали
-                if (field[i][j] == sym) {
-                    countHorizontal++;
-                    if (countHorizontal == WINCOUNT) {
-                        return true;
-                    }
+                //по горизонтали
+                if (horizontalChecker(i, j, sym) == WINCOUNT) {
+                    return true;
                 }
-
-                //Победа по вертикали
-                if (field[j][i] == sym) {
-                    countVertical++;
-                    if (countVertical == WINCOUNT) {
-                        return true;
-                    }
+                //по вертикали
+                if (verticalChecker(i, j, sym) == WINCOUNT) {
+                    return true;
+                }
+                //по верхней диагонали
+                if (diagonallUpChecker(i, j, sym) == WINCOUNT) {
+                    return true;
+                }
+                //по нижней диагонали
+                if (diagonalDownChecker(i, j, sym) == WINCOUNT) {
+                    return true;
                 }
             }
         }
-
-            // Победа правого верхненго угла
-            for (int i = 0; i < SIZELINE; i++) {
-                for (int j = 0; j < SIZELINE; j++) {
-                    int k = j + i;
-                    if (k < SIZELINE) {
-                        if (field[i][k] == sym) {
-                            coutSmallTopCrossLine++;
-                            if (coutSmallTopCrossLine == WINCOUNT) {
-                                return true;
-                            }
-                        }
-                    }
-                }
-            }
-
-            // Победа левого нижнего угла
-            for (int i = 1; i < SIZELINE; i++) {
-                for (int j = 0; j < SIZELINE; j++) {
-                    int k = j + i;
-                    if (k < SIZELINE) {
-                        if (field[k][i] == sym) {
-                            coutSmallBottomCrossLine++;
-                            if (coutSmallBottomCrossLine == WINCOUNT) {
-                                return true;
-                            }
-                        }
-                    }
-                }
-            }
-
-            // Победа правого нижнего угла
-            for (int i = 0; i < SIZELINE; i++) {
-                for (int j = 0; j < SIZELINE; j++) {
-                    int k = (SIZELINE - 1) - j;
-                    int l = i + j;
-                    if (k >= 0 && l < SIZELINE) {
-                        if (field[l][k] == sym) {
-                            coutSmallBottomAnotherCrossLine++;
-                            if (coutSmallBottomAnotherCrossLine == WINCOUNT) {
-                                return true;
-                            }
-                        }
-                    }
-                }
-            }
-
-            // Победа левого верхнего угла
-            for (int i = 1; i < SIZELINE; i++) {
-                for (int j = 0; j < SIZELINE; j++) {
-                    int k = (SIZELINE - 1) - i - j;
-                    if (k >= 0) {
-                        if (field[j][k] == sym) {
-                            coutSmallTopAnotherCrossLine++;
-                            if (coutSmallTopAnotherCrossLine == WINCOUNT) {
-                                return true;
-                            }
-                        }
-                    }
-                }
-            }
-
-            return false;
-        }
+        return false;
+    }
 
     //13. Проверка заполненения поле фишками
     public static boolean fullField() {
